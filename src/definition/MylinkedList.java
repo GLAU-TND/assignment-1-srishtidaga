6bf9c42bc491ce4d2ebc59ca2b26db8673e1a395
srishtidaga <srishti.daga_cs18@gla.ac.in>
2020-02-22 18:29:48 +0530

    make constructor in Node class

--- src/definition/MylinkedList.java
+++ src/definition/MylinkedList.java
@@ -4,5 +4,12 @@ public class MylinkedList {
     private static class Node<E> {
         private E data;
         private Node<E> next;
+
+
+        private Node(E data, Node<E> next) {
+            this.data = data;
+            this.next = next;
+        }
     }
-}
+    }
+
