7ed39dfcd6f0cf9c3f20b6d474911baf64c949ba
srishtidaga <srishti.daga_cs18@gla.ac.in>
2020-02-22 18:43:18 +0530

    make addFirst method

--- src/definition/MylinkedList.java
+++ src/definition/MylinkedList.java
@@ -1,6 +1,31 @@
 package definition;
 
-public class MylinkedList {
+import adt.ConcADT;
+
+public class MylinkedList<E> implements ConcADT<E> {
+    private Node head = null;
+    private int size = 0;
+
+    private void addFirst(E item) {
+        head = new Node<>(item, head);
+        size++;
+    }
+
+    @Override
+    public void add(Object item) {
+
+    }
+
+    @Override
+    public E remove() {
+        return null;
+    }
+
+    @Override
+    public E getData(int index) {
+        return null;
+    }
+
     private static class Node<E> {
         private E data;
         private Node<E> next;
