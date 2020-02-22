a140390bab090c37df4d0c5cfc4a8e0412891275
srishtidaga <srishti.daga_cs18@gla.ac.in>
2020-02-22 14:17:09 +0530

    override the methods of MyConcADT class in MyContact class

--- src/definition/MyContact.java
+++ src/definition/MyContact.java
@@ -3,4 +3,23 @@ package definition;
 import adt.MyConcADT;
 
 public class MyContact implements MyConcADT {
+    @Override
+    public void addContact() {
+
+    }
+
+    @Override
+    public void viewContacts() {
+
+    }
+
+    @Override
+    public void deleteContact() {
+
+    }
+
+    @Override
+    public void searchContact() {
+
+    }
 }
