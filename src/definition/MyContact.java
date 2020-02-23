d81596a5ba41bc593648a332803aaf6f6b97e9eb
srishtidaga <srishti.daga_cs18@gla.ac.in>
2020-02-23 17:34:02 +0530

    add getFirstName method

--- src/definition/MyContact.java
+++ src/definition/MyContact.java
@@ -7,6 +7,13 @@ import java.util.Scanner;
 public class MyContact implements MyConcADT {
     Scanner sc = new Scanner(System.in);
 
+    private String getFirstName() {
+        System.out.println("Please Enter the name of the Person");
+        System.out.print("FirstName: ");
+        String FirstName = sc.next();
+        return FirstName;
+    }
+
     @Override
     public void addContact() {
 
