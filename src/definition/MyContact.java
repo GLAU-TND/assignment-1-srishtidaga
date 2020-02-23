4d108be113efb6d3e0820b2db6085de1984a9ced
srishtidaga <srishti.daga_cs18@gla.ac.in>
2020-02-23 18:24:38 +0530

    add GetEmail method

--- src/definition/MyContact.java
+++ src/definition/MyContact.java
@@ -61,6 +61,29 @@ public class MyContact implements MyConcADT {
 
     }
 
+    private String GetEmail() {
+        String Email = null;
+        while (true) {
+            System.out.println("Do you want to add an email ? (y/n) :");
+            String a = sc.next();
+            char at = a.charAt(0);
+            if (a.length() > 1) {
+                System.out.println("Please Enter a Valid Input i.e.,y(lowerCase) for Yes or n(lowerCase for NO)");
+                continue;
+            }
+            if (at == 'y') {
+                System.out.println("Email Address: ");
+                Email = sc.next();
+            } else if (at == 'n') {
+                break;
+            } else {
+                System.out.println("Please Enter a Valid Input i.e.,y(lowerCase) for Yes or n(lowerCase for NO)");
+            }
+
+        }
+        return Email;
+    }
+
     @Override
     public void addContact() {
 
